654ade954dd3620d46a69dfc1bddc7a35dcad0b1
Olga Kosoplecheva <olgakosoplecheva@gmail.ru>
2022-10-14 20:38:16 +0300

    Решение задачи 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,4 +1,18 @@
 ﻿// 1. на вход принимает число
 // 2. выдаёт, является ли число чётным (делится ли оно на два без остатка).
 // Например: 4 -> да; -3 -> нет; 7 -> нет
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.Write(" четное");
+}
+else
+{
+Console.Write("Число ");
+Console.Write(number);
+Console.Write(" не четное");
+}
